3302bc30468371c38ca96c233d9cf39cf9bed71e
Yorkis1 <121751635+Yorkis1@users.noreply.github.com>
2024-04-08 15:25:32 +0300

    Add slow typing
    The func print symbols with some speed

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ user rolls it.
 """
 
 import random
+import sys
+import time
+
+typing_speed = 500 #wpm
+
+def slow_type(t):
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(random.random()*10.0/typing_speed)
 
 diceStyle = input("What style do you want (numbers, () - parenthesis)?\n")
 while True:
@@ -16,29 +26,29 @@ while True:
 
         if diceStyle == 'numbers':
             if x == 1:
-                print("\n -------\n|       |\n|   1   |\n|       |\n -------\n")
+                slow_type("\n -------\n|       |\n|   1   |\n|       |\n -------\n")
             elif x == 2:
-                print("\n -------\n| 2     |\n|       |\n|     2 |\n -------\n")
+                slow_type("\n -------\n| 2     |\n|       |\n|     2 |\n -------\n")
             elif x == 3:
-                print("\n -------\n| 3     |\n|   3   |\n|     3 |\n -------\n")
+                slow_type("\n -------\n| 3     |\n|   3   |\n|     3 |\n -------\n")
             elif x == 4:
-                print("\n -------\n| 4   4 |\n|       |\n| 4   4 |\n -------\n")
+                slow_type("\n -------\n| 4   4 |\n|       |\n| 4   4 |\n -------\n")
             elif x == 5:
-                print("\n -------\n| 5   5 |\n|   5   |\n| 5   5 |\n -------\n")
+                slow_type("\n -------\n| 5   5 |\n|   5   |\n| 5   5 |\n -------\n")
             elif x == 6:
-                print("\n -------\n| 6   6 |\n| 6   6 |\n| 6   6 |\n -------\n")
+                slow_type("\n -------\n| 6   6 |\n| 6   6 |\n| 6   6 |\n -------\n")
         elif diceStyle == '()' or diceStyle == "parenthesis":
             if x == 1:
-                print("\n --------\n|        |\n|   ()   |\n|        |\n --------\n")
+                slow_type("\n --------\n|        |\n|   ()   |\n|        |\n --------\n")
             elif x == 2:
-                print("\n --------\n| ()     |\n|        |\n|     () |\n --------\n")
+                slow_type("\n --------\n| ()     |\n|        |\n|     () |\n --------\n")
             elif x == 3:
-                print("\n --------\n| ()     |\n|   ()   |\n|     () |\n --------\n")
+                slow_type("\n --------\n| ()     |\n|   ()   |\n|     () |\n --------\n")
             elif x == 4:
-                print("\n --------\n| ()  () |\n|        |\n| ()  () |\n --------\n")
+                slow_type("\n --------\n| ()  () |\n|        |\n| ()  () |\n --------\n")
             elif x == 5:
-                print("\n --------\n| ()  () |\n|   ()   |\n| ()  () |\n --------\n")
+                slow_type("\n --------\n| ()  () |\n|   ()   |\n| ()  () |\n --------\n")
             elif x == 6:
-                print("\n --------\n| ()  () |\n| ()  () |\n| ()  () |\n --------\n")
+                slow_type("\n --------\n| ()  () |\n| ()  () |\n| ()  () |\n --------\n")
     else:
         break
